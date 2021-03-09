8bc9d66f712d630f9e95059a861c495327fec447
Sean Tarzy <seantarzy@gmail.com>
2021-03-09 13:06:01 -0500

    got everything working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import students from './students.json'
+ 
+class App extends React.Component{
 
-function App() {
+  state= {
+    students: null
+  
+  }
+
+  componentDidMount =()=>{
+    console.log(students)
+    this.setState({students})
+    this.groupByGrade()
+    this.getLowestAvgeragePerGrade()
+      }
+
+      groupByGrade = ()=>{
+        let studentsByGrade = [...students].sort((a,b)=>a.grade - b.grade)
+          return studentsByGrade
+      }
+      getAverageOfGrades = (student)=>{
+        let {scores} = student
+        let total = 0
+        for(let i=0;i<scores.length; i++){
+
+          total += scores[i]["value"]
+        }
+        return total/(scores.length)
+      }
+
+      getStudentAverages = (studentArray)=>{
+         let studentAvgs = studentArray.map((student) => {
+           let avgGrade = this.getAverageOfGrades(student);
+           student["avg"] = avgGrade;
+           return student;
+         });
+         return studentAvgs
+      }
+
+      getLowestAvgeragePerGrade = ()=>{
+        let studentsByGrade = this.groupByGrade()
+        let studentAvgs = this.getStudentAverages(studentsByGrade)
+        let lowestAvgPerGrade = []
+        let currentLowStudent = studentAvgs[0]
+        for(let i=1; i<studentAvgs.length; i++){
+          if (studentAvgs[i]["grade"] > currentLowStudent["grade"] || i === studentAvgs.length-1){
+            lowestAvgPerGrade.push(currentLowStudent)
+            currentLowStudent = studentAvgs[i]
+          }
+            else if (studentAvgs[i]["avg"] < currentLowStudent["avg"]) {
+              currentLowStudent = studentAvgs[i]
+            }
+        }
+        return lowestAvgPerGrade
+      }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +75,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
